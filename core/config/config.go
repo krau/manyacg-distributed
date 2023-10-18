149d17fb8b91d76bc9618f836c833475774dfa11
krau <me@krau.top>
2023-10-18 09:36:24 +0800

    chore: update config

--- core/config/config.go
+++ core/config/config.go
@@ -5,7 +5,25 @@ import (
 )
 
 type tomlConfig struct {
-	Database database
+	App      appConfig `mapstructure:"app" toml:"app" yaml:"app" json:"app"`
+	Database database  `mapstructure:"database" toml:"database" yaml:"database" json:"database"`
+}
+
+type appConfig struct {
+	Log   logConfig   `mapstructure:"log" toml:"log" yaml:"log" json:"log"`
+	Debug bool        `mapstructure:"debug" toml:"debug" yaml:"debug" json:"debug"`
+	Azure azureConfig `mapstructure:"azure" toml:"azure" yaml:"azure" json:"azure"`
+}
+
+type logConfig struct {
+	Level     string `mapstructure:"level" toml:"level" yaml:"level" json:"level"`
+	FilePath  string `mapstructure:"file_path" toml:"file_path" yaml:"file_path" json:"file_path"`
+	BackupNum uint   `mapstructure:"backup_num" toml:"backup_num" yaml:"backup_num" json:"backup_num"`
+}
+
+type azureConfig struct {
+	BusConnectionString string `mapstructure:"bus_connection_string" toml:"bus_connection_string" yaml:"bus_connection_string" json:"bus_connection_string"`
+	Queue               string `mapstructure:"queue" toml:"queue" yaml:"queue" json:"queue"`
 }
 
 type database struct {
@@ -25,6 +43,10 @@ func init() {
 	viper.SetConfigType("toml")
 	viper.SetEnvPrefix("PICCOLLECTOR")
 	viper.AutomaticEnv()
+	viper.SetDefault("app.debug", false)
+	viper.SetDefault("app.log.level", "info")
+	viper.SetDefault("app.log.file_path", "./logs/core.log")
+	viper.SetDefault("app.log.backup_num", 7)
 	err := viper.ReadInConfig()
 	if err != nil {
 		panic(err)
